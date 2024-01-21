c0f0cfcd0757e2f829c846c1ffae1a5762cac2ae
Chris <chris.carr@cobersolutions.com>
2024-01-21 15:54:57 -0500

    first

---
+++ resources/views/pizza.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Your Label Pizza</title>
+    </head>
+    <body>
+        <div>Welcome to Your Label Pizza</div>
+        <div>We will serve all your pizza technology needs from public web sites, ordering, instore POS, directions, remote POS at the door, employee scheduling, payroll, digital displays, inventory managment, inventory ordering, customer tracking, sales, reordering, alergins</div>
+    </body>
+</html>
